46f6815739cb5b145446751c3bfc40dd41d68d07
cgrain <coengruijt@gmail.com>
2013-09-19 14:11:31 +0200

    kerk opgeslagen

--- page/buildings.user.js
+++ page/buildings.user.js
@@ -3,11 +3,17 @@ else if (location.href.indexOf('mode=buildings') > -1) {
 	var header = $("tr:contains('Bouwopdrachten')", "#buildings_table");
 	var trheader = $(header).find("th");
 	var indexmuur = 0;
+	var indexkerk = 0;
 	var muurtje = JSON.parse(localStorage["CTmuur"]);
-	if (muurtje === undefined) { stackie = {};} 
+	var kerkje = JSON.parse(localStorage["CTkerk"]);
+	if (muurtje === undefined) { muurtje = {};} 
+	if (kerkje === undefined) { kerkje = {};} 
 	trheader.each(function(index, value) {//Welke kolom is die van de muur?
 		if (/wall/.test($(value).html())) {
 			indexmuur = index;
+		}
+		if (/church/.test($(value).html())) {
+		indexkerk= index;
 		}
 	});
 	rows.each(function() {
@@ -15,6 +21,17 @@ else if (location.href.indexOf('mode=buildings') > -1) {
 			var tds = $(this).find("td");
 			var muurlevel = $(tds[indexmuur]).text();
 			muurtje[coords] = muurlevel;
+			var kerklevel  = $(tds[indexkerk]).text();
+			var eerstekerk = $(tds[indexkerk+1]).text();
+			if (eerstekerk) {
+				kerkje.push(coords);
+			}
+			else if (kerklevel != 0) {
+				kerkje.push(coords);
+			}
+			
+			
 	});
 	localStorage["CTmuur"] = JSON.stringify(muurtje);
+	localStorage["CTkerk"] = JSON.stringify(kerkje);
 }
